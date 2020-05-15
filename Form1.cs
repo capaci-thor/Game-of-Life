f36c02f103fc3091c815f9867b3d1c9f43c23890
Jose Eduardo O. T <rotjeot@gmail.com>
2020-05-15 14:01:36 -0500

    Recorrido de imagen y agrgado de casos de celulas vivas y muertas

--- Form1.cs
+++ Form1.cs
@@ -25,22 +25,96 @@ namespace game_of_life
             InitializeComponent();
         }
 
+        /* Reglas
+         * Una célula muerta con exactamente 3 células vecinas vivas "nace" (es decir, al turno siguiente estará viva).
+         * Una célula viva con 2 o 3 células vecinas vivas sigue viva, en otro caso muere (por "soledad" o "superpoblación").
+         * 
+         * Celulas blancas -> Vivas
+         * Celulas negras -> Muertas
+         */
         public void Draw()
         {
+            for(int i = 0; i < img.Width - 1; i++)
+            {
+                for(int j = 0; j < img.Width -1; j++)
+                {
+                    if(Status(i, j))//Si es negro
+                    {
+                        if(Count(j, i) != 2 || Count(j, i) != 3)
+                        {
+                            ;
+                        }
+                        else
+                        {
+                            img.Data[j, i, 0] = 255; //Muere
+                        }
+                    }
+                    else
+                    {
+                        if(Count(j, i) == 3)
+                        {
+                            img.Data[j, i, 0] = 0;
+                        }
+                    }
+
+
+                }
+            }
             
         }
 
 
-        public bool Status(int x, int y)
+        public bool Status(int x, int y)//Regresa true si celula viva(negra) y false si celula muerta(blanca)
         {
+            if( img.Data[y,x,0] == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             
-            return true;
         }
 
-        public int Count(int x, int y)
+        public int Count(int x, int y) //Regresa el numero de celulas vivas al rededor de una coordenada
         {
 
-            return 0;
+            int count = 0;
+            if (x > 0)
+            {
+                count = img.Data[y, x - 1, 0] == 0 ? count++ : count; //Izquierda
+            }
+            if (x < img.Width)
+            { 
+                count = img.Data[y, x + 1, 0] == 0 ? count++ : count; //Derecha
+            }
+            if (y > 0)
+            {
+                count = img.Data[y - 1, x, 0] == 0 ? count++ : count; // Arriba
+            }
+            if (y < img.Height)
+            {
+                count = img.Data[y + 1, x, 0] == 0 ? count++ : count; // Abajo
+            }
+            if ( y > 0 && x > 0)
+            {
+                count = img.Data[y - 1, x - 1, 0] == 0 ? count++ : count; // Izquierda - arriba
+            }
+            if ( y < img.Height && x < img.Width)
+            {
+                count = img.Data[y - 1, x + 1, 0] == 0 ? count++ : count; // Derecha - arriba
+            }
+            if (y < img.Width && x > 0)
+            {
+                count = img.Data[y + 1, x - 1, 0] == 0 ? count++ : count; // Izquierda - abajo
+            }
+            if (y < img.Height && x < img.Width)
+            {
+                count = img.Data[y + 1, x + 1, 0] == 0 ? count++ : count; // Derecha - abajo
+            }
+
+            return count;
         }
 
         private void Juego_Click(object sender, EventArgs e)
